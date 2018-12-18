11c2ecbb68f5a761ff8ecc7dde67d5cd92f9d1ac
craftlinker <starterbee2@gmail.com>
2018-12-18 13:07:00 -0500

    Search Result Page

---
+++ src/app/search-result-page/search-result-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-result-page',
+  templateUrl: './search-result-page.component.html',
+  styleUrls: ['./search-result-page.component.css']
+})
+export class SearchResultPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

b659c9f6a121c4a6ab0454650017345b26cfd65c
craftlinker <starterbee2@gmail.com>
2018-12-18 13:04:47 -0500

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchResultPageComponent } from './search-result-page/search-result-page.component';
+import { PasswordResetPageComponent } from './password-reset-page/password-reset-page.component';
+import { OutOfStockPageComponent } from './out-of-stock-page/out-of-stock-page.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchResultPageComponent,
+    PasswordResetPageComponent,
+    OutOfStockPageComponent,
+   
   ],
   imports: [
     BrowserModule,

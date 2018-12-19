3f6b295efd7b8cd76d02c0366092a13e926fc942
craftlinker <starterbee2@gmail.com>
2018-12-19 08:35:17 -0500

    Update app.module.ts
    import {HttpClientModule} from '@angular/common/http'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchResultPageComponent } from './search-result-page/search-result-page.component';
@@ -18,7 +18,8 @@ import { OutOfStockPageComponent } from './out-of-stock-page/out-of-stock-page.c
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]

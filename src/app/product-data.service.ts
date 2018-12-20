d0cd839018f6486beb8e249f525a601f2a16c777
craftlinker <starterbee2@gmail.com>
2018-12-20 18:04:25 -0500

    Update product-data.service.ts
    Testing Changes

--- src/app/product-data.service.ts
+++ src/app/product-data.service.ts
@@ -1,18 +1,32 @@
 import { Injectable } from '@angular/core';
-import { HttpClientModule, HttpHeaders } from '@angular/common/';
+import { HttpClientModule, HttpHeaders, HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Product } from './product';
 
+
+
+@Injectable()
+export class ServiceNameService {
+  
+  constructor(private http: HttpClient) { }
+
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductDataService {
-  url: string = ""
-
-  getProduct(): Observable<Product>{
-    return this.http.get<Product[]>(this.url)
-  }
 
   constructor(private http: HttpClient) { }
+
+  url: string = "https://ec2-3-17-5-151.us-east-2.compute.amazonaws.com";
+
+  
+
+
+  getProduct(): Observable<Product>{
+    return this.http.get<Product>(this.url)
+    
+  }
+
 }

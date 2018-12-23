b55959feda9930ee4ee7d9bce171bc22093942cd
craftlinker <starterbee2@gmail.com>
2018-12-23 13:41:48 -0500

    Update product-data.service.ts
    added HttpHeaders

--- src/app/product-data.service.ts
+++ src/app/product-data.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClientModule, HttpHeaders, HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {  HttpHeaders, HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
 import { Product } from './product';
 
 
@@ -19,13 +19,13 @@ export class ProductDataService {
 
   constructor(private http: HttpClient) { }
 
-  url: string = "https://ec2-3-17-5-151.us-east-2.compute.amazonaws.com";
+  url: string = "https://localhost:3000/products";
 
   
 
 
-  getProduct(): Observable<Product>{
-    return this.http.get<Product>(this.url)
+  getProducts(): Observable<Product[]>{
+    return this.http.get<Product[]>(this.url);
     
   }
 

4e0efcd31d400d24f32cf07dbf5cf21676c867d7
craftlinker <starterbee2@gmail.com>
2018-12-23 13:40:50 -0500

    Update home-page-default.module.ts
    added the HttpClientModule

--- src/app/home-page-default/home-page-default.module.ts
+++ src/app/home-page-default/home-page-default.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageDefaultComponent } from './Components/home-page-default.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { AppModule } from '../app.module';
+
 @NgModule({
   declarations: [HomePageDefaultComponent],
   imports: [
-    CommonModule
+    CommonModule, HttpClientModule
   ]
 })
 export class HomePageDefaultModule { }
